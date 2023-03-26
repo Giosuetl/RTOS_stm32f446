afbfb77a6f61aa87b78671372d26444d1b47e2b1
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-26 12:25:50 -0600

    Add files via upload

---
+++ STM32_FreeRTOS_Led_and_button_IT_/Debug/Third-Party/FreeRTOS/org/Source/portable/MemMang/heap_4.d
@@ -0,0 +1,41 @@
+Third-Party/FreeRTOS/org/Source/portable/MemMang/heap_4.o: \
+ ../Third-Party/FreeRTOS/org/Source/portable/MemMang/heap_4.c \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Config/FreeRTOSConfig.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/Config/Global.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/list.h
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Config/FreeRTOSConfig.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/Config/Global.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/FreeRTOS/org/Source/include/list.h:
---
+++ STM32_FreeRTOS_Led_and_button_IT_/Debug/Third-Party/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,8 @@
+Third-Party/SEGGER/SEGGER/SEGGER_RTT.o: \
+ ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.c \
+ ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
+
+../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Led_and_button_IT_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
