8fc0f4c11309db47b1c2adcae4d86f06e45af88a
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-27 12:01:00 -0600

    Delete STM32_FreeRTOS_Task_Delete/Third-Party directory

--- STM32_FreeRTOS_Task_Delete/Third-Party/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-Third-Party/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.c \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Task_Delete/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Task_Delete/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
