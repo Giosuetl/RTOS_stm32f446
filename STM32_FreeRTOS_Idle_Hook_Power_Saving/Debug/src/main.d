8af90bcd21a3591dda78376662c75b168c4e5130
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-29 09:01:18 -0600

    Add files via upload

---
+++ STM32_FreeRTOS_Idle_Hook_Power_Saving/Debug/src/main.d
@@ -0,0 +1,157 @@
+src/main.o: ../src/main.c \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/device/stm32f4xx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cm4.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmInstr.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmFunc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmSimd.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/misc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dma2d.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_ltdc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_sai.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_qspi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_cec.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Config/FreeRTOSConfig.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/Config/Global.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/list.h
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/device/stm32f4xx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cm4.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmInstr.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmFunc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core/core_cmSimd.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_dma2d.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_ltdc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_sai.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_qspi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc/stm32f4xx_cec.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Config/FreeRTOSConfig.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/Config/Global.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/list.h:
