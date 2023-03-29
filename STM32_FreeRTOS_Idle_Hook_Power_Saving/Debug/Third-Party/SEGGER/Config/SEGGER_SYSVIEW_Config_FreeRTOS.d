02115d2b92c3c85b1a1dd9ebbafe45acd6cf0c69
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-29 09:05:35 -0600

    Add files via upload

---
+++ STM32_FreeRTOS_Idle_Hook_Power_Saving/Debug/Third-Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d
@@ -0,0 +1,35 @@
+Third-Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o: \
+ ../Third-Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Config/FreeRTOSConfig.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/Config/Global.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Config/FreeRTOSConfig.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/SEGGER/Config/Global.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
