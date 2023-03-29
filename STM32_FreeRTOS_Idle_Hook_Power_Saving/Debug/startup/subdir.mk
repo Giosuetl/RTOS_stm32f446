f6f393f81df29e5539bc15134a31dc896a12f607
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-29 09:04:27 -0600

    Add files via upload

---
+++ STM32_FreeRTOS_Idle_Hook_Power_Saving/Debug/startup/subdir.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../startup/startup_stm32.s 
+
+OBJS += \
+./startup/startup_stm32.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/%.o: ../startup/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo $(PWD)
+	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -I"C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/StdPeriph_Driver/inc" -I"C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/inc" -I"C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/device" -I"C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_Idle_Hook_Power_Saving/CMSIS/core" -g -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
