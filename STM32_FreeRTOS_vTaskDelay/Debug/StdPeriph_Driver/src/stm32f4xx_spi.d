309d7fc2611ff6373dedd02a7dda2616ec9e8b00
Giosu <63215931+Giosuetl@users.noreply.github.com>
2023-03-29 04:59:27 -0600

    Add files via upload

---
+++ STM32_FreeRTOS_vTaskDelay/Debug/StdPeriph_Driver/src/stm32f4xx_spi.d
@@ -0,0 +1,125 @@
+StdPeriph_Driver/src/stm32f4xx_spi.o: \
+ ../StdPeriph_Driver/src/stm32f4xx_spi.c \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/device/stm32f4xx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cm4.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmInstr.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmFunc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmSimd.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/misc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dma2d.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_ltdc.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_sai.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_qspi.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_cec.h \
+ C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rcc.h
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/device/stm32f4xx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cm4.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmInstr.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmFunc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/core/core_cmSimd.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_dma2d.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_ltdc.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_sai.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_qspi.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_fmpi2c.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_spdifrx.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_cec.h:
+
+C:/Users/Nestor/Documents/embedded/freeRtos/RTOS_workspace/STM32_FreeRTOS_vTaskDelay/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
